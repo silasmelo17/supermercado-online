f957053d36383bc460560bb91c57038953ceab66
silasmelo17 <silasmelo1967@gmail.com>
2021-10-11 18:10:52 -0300

    update

--- src/store/authentication/thunks.ts
+++ src/store/authentication/thunks.ts
@@ -1,29 +1,41 @@
 
 import axios from '../../config/axios.config';
 
-import { AxiosResponse } from 'axios';
-
 import { ThunkDispatch } from 'redux-thunk';
 import { AnyAction } from 'redux';
 
 import GlobalState from '../../types/GlobalState';
-import AxiosResponseProducts from '../../types/AxiosResponseProducts';
 
 import * as ActionsAuthentication from './actions';
 import AxiosResponseAuthentication from '../../types/AxiosResponseAuthentication';
 
 
 
+interface AxiosRequestAuthentication {
+    headers: {
+        token: string
+    },
+    data: {
+        email: string,
+        password: string
+    }
+}
+
+
 export const userAuthentication = ( email: string, password: string ) => 
     async (dispatch: ThunkDispatch<GlobalState, void, AnyAction>, getState: () => GlobalState ) => {
         const { token } = getState().authentication;
 
-        const { status, data } = await axios.post<undefined, AxiosResponseAuthentication >(`/user/logged/`,
-            undefined,
-            { token }
-        );
+        const { status, data } = await axios.post<AxiosRequestAuthentication, AxiosResponseAuthentication >(`/user/signin/`,{
+            headers: { token },
+            data: {
+                email, password
+            }   
+        });
+
+        console.log(status, data)
         
 
-        dispatch( ActionsAuthentication( data.auth, data.token ) );
+        dispatch( ActionsAuthentication.userAuthentication( data.auth, data.token ) );
     }
 
