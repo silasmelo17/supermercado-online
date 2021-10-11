771ae88f44f65dceebaf01c641f1936934767ad0
Silas de Melo Araujo <silasmelo1967@gmail.com>
2021-10-11 00:19:43 -0300

    create type

---
+++ src/types/User.ts
@@ -0,0 +1,12 @@
+
+interface User {
+    id?: number,
+    name?: string,
+    lastName?: string,
+    email?: string,
+    phone?: string,
+    cpf?: string
+    birthday?: Date,
+}
+
+export default User;
