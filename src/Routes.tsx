c68933f72ffdeeb11385f712ea47a85b4043fe4f
silasmelo17 <silasmelo1967@gmail.com>
2021-10-11 15:52:55 -0300

    update Router

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,26 +1,32 @@
 
-import React from 'react';
-
 import {
   BrowserRouter,
   Switch,
   Route,
 } from 'react-router-dom';
 
-import Products from './pages/Products';
-import ProductsByName from './pages/ProductsByName';
+import Header from './components/Header';
+
+import Products from './pages/Products/';
+import ProductsByName from './pages/ProductsByName/';
+import SignIn from './pages/SignIn';
 
 
 
 function Routes() {
     return(
         <BrowserRouter>
+            <Header />
+
             <Switch>
                 <Route path="/products/name/:name">
                     <ProductsByName />
                 </Route>
                 <Route path="/products">
                     <Products />
+                </Route>
+                <Route path="/signin">
+                    <SignIn />                    
                 </Route>
             </Switch>
         </BrowserRouter>
