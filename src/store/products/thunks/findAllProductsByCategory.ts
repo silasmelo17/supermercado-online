141c7d6b979ab59b2890c4e2e078150ae1e3a210
silasmelo17 <silasmelo1967@gmail.com>
2021-10-11 18:11:16 -0300

    find products by category

---
+++ src/store/products/thunks/findAllProductsByCategory.ts
@@ -0,0 +1,27 @@
+
+import { AnyAction } from 'redux';
+import { ThunkDispatch } from "redux-thunk";
+
+import axios from "../../../config/axios.config";
+
+import ActionsProducts from "../actions";
+import AxiosResponseProducts from "../../../types/AxiosResponseProducts";
+import GlobalState from "../../../types/GlobalState";
+
+
+
+function findAllProductsByCategory( id: number ) {
+    return async (dispatch: ThunkDispatch<GlobalState, void, AnyAction>, getState: () => GlobalState  ) => {
+        const { page, limit } = (getState()).products;
+        const { token } = (getState()).authentication;
+
+        const { data } = await axios.get<AxiosResponseProducts>( `/products/category/${id}`, {
+            headers: { token },
+            params: { limit, page }
+        });
+
+        dispatch(ActionsProducts.setProducts({...data}) );
+    }
+}
+
+export default findAllProductsByCategory;
