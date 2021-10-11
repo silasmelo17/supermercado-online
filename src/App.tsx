b698c9d34570566ee6bf6e8604f15a9cc8dd3ee5
Silas de Melo Araujo <silasmelo1967@gmail.com>
2021-10-11 00:21:40 -0300

    add redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
+import { Provider } from 'react-redux';
+
+import store from './store';
+
 import GlobalStyles from './globalStyles';
 import Header from './components/Header';
 
@@ -9,12 +13,12 @@ import Routes from './Routes';
 
 function App() {
   return (
-    <>
+    <Provider store={store} >
       <GlobalStyles />
+      
       <Header />
-
       <Routes />
-    </>
+    </Provider>
   );
 }
 
--- src/components/ListProducts/index.tsx
+++ src/components/ListProducts/index.tsx
@@ -1,22 +1,44 @@
 
-import TypeProduct from '../../types/TypeProduct';
+import { connect, ConnectedProps  } from 'react-redux'; 
+
+import GlobalState from '../../types/GlobalState';
+
 import Product from '../Product';
 
 import { ProductsContainer } from './styles';
 
 
-interface ListProductsProps {
-    products: TypeProduct[]
-}
+
+const mapStateToProps = (state: GlobalState) => ({
+    products: state.products.data
+});
+
+const connector = connect(mapStateToProps );
 
 
 
-function ListProducts({ products }: ListProductsProps ) {
+type PropsFromRedux = ConnectedProps<typeof connector>
+
+interface Props extends PropsFromRedux {}
+
+
+
+
+function ListProducts(props: Props) {
+
+    // const LoadingProducts = () => props.products.length === 0
+    //     ? (<Loading size={32} />)
+    //     : (<ProductsContainer>
+    //         { props.products.map( (product, i) => 
+    //             <Product key={`product___${i}`} {...product} /> ) }
+    //     </ProductsContainer>)
+    
     return(
         <ProductsContainer>
-            { products.map( (product, i) => <Product key={`product___${i}`} {...product} /> ) }
+            { props.products.map( (product, i) => 
+                <Product key={`product___${i}`} {...product} /> ) }
         </ProductsContainer>
-    )
+    );
 }
 
-export default ListProducts;
+export default connector(ListProducts);
