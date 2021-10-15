d11bc70211e9d0ece2e0e6bda9545b4f42494526
silasmelo17 <silasmelo1967@gmail.com>
2021-10-15 16:54:20 -0300

    update account type

--- src/types/Account.ts
+++ src/types/Account.ts
@@ -1,12 +1,14 @@
 
 import Favorites from "./Favorites";
 import User from "./User";
+import Address from './Address';
 
 
 
 interface Account {
-    user?: User;
-    favorites: Favorites
+    user: User;
+    favorites: Favorites,
+    addresses: Address[]
 }
 
 export default Account;
