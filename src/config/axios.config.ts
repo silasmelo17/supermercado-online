7bb35ecfe27023ced95a1933ce924de41b52f7a5
Silas de Melo Araujo <silasmelo1967@gmail.com>
2021-10-15 21:48:06 -0300

    update

--- src/config/axios.config.ts
+++ src/config/axios.config.ts
@@ -4,8 +4,7 @@ import axios from 'axios';
 
 
 const api = axios.create({
-    // baseURL: "https://supermercado-online-api.herokuapp.com/api",
-    baseURL: 'http://localhost:3000/api'
+    baseURL: process.env.URL_API || 'http://localhost:3000/api'
 });
 
 export default api;
