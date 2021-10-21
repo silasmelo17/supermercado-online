c972515ccfe371900eb57a0a88d02fbf91cb0cd6
silasmelo17 <silasmelo1967@gmail.com>
2021-10-21 17:56:56 -0300

    update store

--- src/store/favorites/thunks.ts
+++ src/store/favorites/thunks.ts
@@ -14,68 +14,81 @@ import GlobalState from '../../types/reduxState/GlobalState';
 
 
 
-export const findFavoritesProducts = () => 
-    async (dispatch: ThunkDispatch<GlobalState, void, AnyAction>, getState: () => GlobalState ) => {
-        const state = getState();
+export const findFavoritesProducts = () =>
+    async (dispatch: ThunkDispatch<GlobalState, void, AnyAction>, getState: () => GlobalState) => {
+        try {
+            const state = getState();
+            const { token } = state.authentication;
 
-        const { token } = state.authentication;
-        
-        const {data} = await axios.get<Favorites>( `/favorites/user/`, {
-            headers: { token },
-            params: {
-                page: state.account?.favorites?.page || 1,
-                limit: state.account?.favorites?.limit || 10,
-                fields: [ 'id', 'name', 'image_src' ]
-            }
-        });
+            const { data } = await axios.get<Favorites>(`/favorites/user/`, {
+                headers: { token },
+                params: {
+                    page: state.account?.favorites?.page || 1,
+                    limit: state.account?.favorites?.limit || 10,
+                    fields: ['id', 'name', 'image_src']
+                }
+            });
 
-        dispatch( FavoritesAction.setFavorites(data) );
+            dispatch(FavoritesAction.setFavorites(data));
+        } catch (e) {
+            const err = e as AxiosError<{ message: string }>;
+            alert(err.response?.data.message);
+        }
     }
 
-export const toggleFavoriteProduct = ( id: number, index: number ) => 
-    async (dispatch: ThunkDispatch<GlobalState, void, AnyAction>, getState: () => GlobalState ) => {
-        const { token } = getState().authentication;
+export const toggleFavoriteProduct = (id: number, index: number) =>
+    async (dispatch: ThunkDispatch<GlobalState, void, AnyAction>, getState: () => GlobalState) => {
+        try {
+            const { token } = getState().authentication;
 
-        const {data}: AxiosResponse<any> = await axios
-            .post<any, any>( `/favorites/product/${id}`, {}, { headers: {token}} )
-        
-        console.log(data);
+            const { data }: AxiosResponse<any> = await axios
+                .post<any, any>(`/favorites/product/${id}`, {}, { headers: { token } });
 
-        dispatch( ProductsAction
-            .updateProductByIndex( index, { id, Favorites: data }) );
+            dispatch(ProductsAction
+                .updateProductByIndex(index, { id, Favorites: data }));
+        }catch(e) {
+            const err = e as AxiosError<{ message: string }>;
+            alert(err.response?.data.message);
+        }
     }
 
 
-    
-export const removeFavoriteFromList = ( id: number, index: number ) => 
-    async (dispatch: ThunkDispatch<GlobalState, void, AnyAction>, getState: () => GlobalState ) => {
-        const state = getState();
 
-        const { token } = state.authentication;
+export const removeFavoriteFromList = (id: number, index: number) =>
+    async (dispatch: ThunkDispatch<GlobalState, void, AnyAction>, getState: () => GlobalState) => {
+        try {
+            const state = getState();
+            const { token } = state.authentication;
 
-        const result: AxiosResponse<any> = await axios.post<any, any>( `/favorites/product/${id}`, {}, { headers: {token}} )
-            .catch( (err: AxiosError) => {} );
+            const result: AxiosResponse<any> = await axios.post<any, any>(`/favorites/product/${id}`, {}, { headers: { token } })
 
-        console.log(result);
-
-        if(result.status === 200 )
-            dispatch(FavoritesAction.removeFavorite(index));
+            if (result.status === 200)
+                dispatch(FavoritesAction.removeFavorite(index));
+        } catch (e) {
+            const err = e as AxiosError<{ message: string }>;
+            alert(err.response?.data.message);
+        }
     }
 
-export const incrementFavorites = () => 
-    async(dispatch: ThunkDispatch<GlobalState, void, AnyAction>, getState: () => GlobalState ) => {
-        const state = getState();
-        const { token } = state.authentication;
-        
-        const {data} = await axios.get<Favorites>( `/favorites/user/`, {
-            headers: { token },
-            params: {
-                page: state.account.favorites.page + 1,
-                limit: state.account?.favorites?.limit || 10,
-                offset: state.account.favorites.data.length,
-                fields: [ 'id', 'name', 'image_src' ]
-            }
-        });
+export const incrementFavorites = () =>
+    async (dispatch: ThunkDispatch<GlobalState, void, AnyAction>, getState: () => GlobalState) => {
+        try {
+            const state = getState();
+            const { token } = state.authentication;
 
-        dispatch(FavoritesAction.incrementFavorites(data));
+            const { data } = await axios.get<Favorites>(`/favorites/user/`, {
+                headers: { token },
+                params: {
+                    page: state.account.favorites.page + 1,
+                    limit: state.account?.favorites?.limit || 10,
+                    offset: state.account.favorites.data.length,
+                    fields: ['id', 'name', 'image_src']
+                }
+            });
+
+            dispatch(FavoritesAction.incrementFavorites(data));
+        } catch (e) {
+            const err = (e as AxiosError<{ message: string }>);
+            alert(err.response?.data.message);
+        }
     }
